c278bbfe101200c23e1a2a136561189af0816e97
TizzySaurus <tizzysaurus@gmail.com>
2024-03-31 13:52:59 +0100

    Add cogs.events.messages for processing message events (currently only on_message)

---
+++ cogs/events/messages.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from discord import Message
+from discord.ext import commands
+
+from bot import MyBot
+from utils.aes import encrypt
+
+
+class MessageEventsHandler(commands.Cog):
+    def __init__(self, bot: MyBot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        print(message)
+        if message.author.bot or not message.guild:
+            # Ignore messages that are from bots or in DMs
+            return
+
+        as_tuple = MessageEventsHandler.message_to_tuple(message)
+        await self.bot.message_batcher.add(as_tuple)
+
+    @staticmethod
+    def message_to_tuple(message: Message) -> tuple[int, int, bytes, str, datetime]:
+        return (message.id, message.author.id, encrypt(message.content or "None").decode(), "", message.created_at)
+
+
+async def setup(bot: MyBot):
+    await bot.add_cog(MessageEventsHandler(bot))
