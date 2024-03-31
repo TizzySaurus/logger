68f0f9d1be344d665c74a99b2c31829b968644ba
TizzySaurus <tizzysaurus@gmail.com>
2024-03-31 13:52:40 +0100

    Create bot.message_batcher & make cog loading recursive

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import logging
 import logging.config
 import os
 import pathlib
+from datetime import datetime
 
 import discord
 import yaml
 from dotenv import load_dotenv
 from discord.ext import commands
 
+from utils.batcher import Batcher
 from utils.postgres import create_postgres_connection
 
 
@@ -30,18 +32,31 @@ class MyBot(commands.Bot):
 
     async def setup_hook(self):
         await self.initialise_postgres()
+
+        self.message_batcher = Batcher[tuple[int, int, bytes, str, datetime]](
+            1_000, "MESSAGE_BATCH_SIZE", self.submit_message_batch
+        )
+
         await self.load_extensions()
 
+    async def submit_message_batch(self, messages_components: list[tuple[int, int, bytes, str, datetime]]):
+        await self.db.execute_sql("INSERT INTO messages VALUES ($1, $2, $3, $4, $5)", messages_components)
+
     async def load_extensions(self):
-        for extension in [file for file in pathlib.Path('cogs').glob('*.py')]:
-            if extension.name == "__init__.py":
+        cogs_dir = pathlib.Path("cogs")
+        for python_file in [file for file in cogs_dir.rglob("*.py")]:
+            if python_file.name == "__init__.py":
                 continue
 
+            extension_parts = python_file.relative_to(cogs_dir).parts
+            extension_file = ".".join(extension_parts)
+            extension_file_name = extension_file[:-3]
+
             try:
-                await bot.load_extension(f"cogs.{extension.stem}")
-                bot.logger.debug(f"Successfully loaded extension {extension.name!r}")
+                await bot.load_extension(f"cogs.{extension_file_name}")
+                bot.logger.debug(f"Successfully loaded extension {extension_file_name!r}")
             except Exception as e:
-                bot.logger.exception(f"Failed to load extension {extension.name!r}:\n{e}\n")
+                bot.logger.exception(f"Failed to load extension {extension_file_name!r}:\n{e}\n")
 
     async def initialise_postgres(self):
         self.db = await create_postgres_connection(self.logger)
