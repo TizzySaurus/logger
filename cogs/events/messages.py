169803a8bb2cd6896eaff753aa65c681533b8bd5
TizzySaurus <tizzysaurus@gmail.com>
2024-04-03 11:43:46 +0100

    Update MessageEventsHandler cog to reflect new batching logic

--- cogs/events/messages.py
+++ cogs/events/messages.py
@@ -1,10 +1,8 @@
-from datetime import datetime
-
 from discord import Message
 from discord.ext import commands
 
 from bot import MyBot
-from utils.aes import encrypt
+from utils.batchers import BatchedMessage
 
 
 class MessageEventsHandler(commands.Cog):
@@ -17,13 +15,8 @@ class MessageEventsHandler(commands.Cog):
         if message.is_system() or message.author.bot or not message.guild:
             # Ignore messages that are system, from bots, or in DMs
             return
-
-        as_tuple = MessageEventsHandler.message_to_tuple(message)
-        await self.bot.message_batcher.add(as_tuple)
-
-    @staticmethod
-    def message_to_tuple(message: Message) -> tuple[int, int, bytes, str, datetime]:
-        return (message.id, message.author.id, encrypt(message.content or "None").decode(), "", message.created_at)
+        batched_message = BatchedMessage.from_discord_message(message)
+        await self.bot.message_batcher.add(batched_message)
 
 
 async def setup(bot: MyBot):
