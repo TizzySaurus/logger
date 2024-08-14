c416cc3e9f291ee1eccfd8f4e3ae4c4b764017de
Marcus Hynfield <marcus@hynfield.org>
2024-08-14 10:30:58 -0400

    Fix undesired messageUpdate logs when posting URLs

--- src/bot/events/messageUpdate.js
+++ src/bot/events/messageUpdate.js
@@ -20,7 +20,7 @@ module.exports = {
     }
     if (!oldMessage) return
     if (newMessage.author.bot && !global.bot.global.guildSettingsCache[newMessage.channel.guild.id].isLogBots()) return
-    if ((newMessage.content === oldMessage.content) && (newMessage.attachments.length === oldMessage.attachment_b64.split("|").filter(Boolean).length)) return // content/attachments didn't change so don't process
+    if ((escape(newMessage.content) === oldMessage.content) && (newMessage.attachments.length === oldMessage.attachment_b64.split("|").filter(Boolean).length)) return // content/attachments didn't change so don't process
     await processMessage(newMessage, oldMessage)
 
     async function processMessage (newMessage, oldMessage) {
